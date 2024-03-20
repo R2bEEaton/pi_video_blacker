fc64c4746b96ac5a2df611e6f3f4803dc6fa1361
Ryan Eaton <scgryan@gmail.com>
2024-03-20 14:38:26 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo "starting..."
 mkdir /home/pi/.config/autostart
 cp autovlc.desktop /home/pi/.config/autostart/autovlc.desktop
 
-git clone https://github.com/goodtft/LCD-show.git
-chmod -R 755 LCD-show
-cd LCD-show/
-sudo ./LCD35-show
+#git clone https://github.com/goodtft/LCD-show.git
+#chmod -R 755 LCD-show
+#cd LCD-show/
+#sudo ./LCD35-show
