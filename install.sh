6ffd1cd0e0c4d33373f164048778e4511a316802
Ryan Eaton <scgryan@gmail.com>
2024-03-25 10:24:27 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,16 +2,11 @@
 
 echo "starting..."
 
-echo "copying autovlc.desktop to autostart directory..."
-mkdir /home/pi/.config/autostart
-cp autovlc.desktop /home/pi/.config/autostart/autovlc.desktop
+echo "copying autoblacker.desktop to autostart directory..."
+mkdir -p /home/pi/.config/autostart
+cp autovlc.desktop /home/pi/.config/autostart/autoblacker.desktop
 
-echo "marking vlcstartup.sh as executable..."
-chmod +x vlcstartup.sh
-
-#git clone https://github.com/goodtft/LCD-show.git
-#chmod -R 755 LCD-show
-#cd LCD-show/
-#sudo ./LCD35-show
+echo "marking blacker_startup.sh as executable..."
+chmod +x blacker_startup.sh
 
 echo "done"
